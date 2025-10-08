9de11c54d51b0f955e0d7fe99ff4848e06c5c3f0
Youssouf <ndanyoussouf@gmail.com>
2025-10-08 09:48:11 +0100

    first commit

--- src/pages/authUsr/Service.tsx
+++ src/pages/authUsr/Service.tsx
@@ -428,7 +428,7 @@ const Service = () => {
             {/* Titre */}
             <h2 className="text-3xl md:text-4xl font-bold text-gray-900 mb-4">
               <span className="border-b-4 border-green-600 pb-1">
-                Our Operational Procedures
+                {t("our.operational")}
               </span>
             </h2>
             <p className="text-gray-600 max-w-3xl mx-auto mb-12">
